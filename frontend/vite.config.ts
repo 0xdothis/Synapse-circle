ce23a9c7c82f98680cdb1d71b3673ce0121999e5
s-olagoke <olagokesikiru06@gmail.com>
2026-07-24 20:08:12 +0100

    feat: backend url issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src")
     } 
   },
+  server: {
+    proxy: {
+      // Redirects local requests starting with /api to your Render backend
+      '/api': {
+        target: 'https://synap-circle-onrender.com',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
   
 
 })
